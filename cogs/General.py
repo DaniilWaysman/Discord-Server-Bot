fb5fee37b1013add5d37a0196c538a41508b7f4e
Daniil Waysman <daniil.waysman@gmail.com>
2025-04-18 01:12:45 +0200

    Добавление информации о сервере

--- cogs/General.py
+++ cogs/General.py
@@ -5,7 +5,8 @@ from disnake.ext import commands
 class General(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
+# 👨‍🦰 Пользователь
     @commands.slash_command(name="пользователь")
     async def user(self, inter: disnake.ApplicationCommandInteraction):
         pass
@@ -21,7 +22,42 @@ class General(commands.Cog):
         user_avatar.set_image(url=target.display_avatar.url)
         
         await inter.response.send_message(embed=user_avatar, ephemeral=True)
+    
+
+#🪐 Сервер
+    @commands.slash_command(name="сервер")
+    async def server_manager(self, inter: disnake.ApplicationCommandInteraction):
+        pass
+    
+    @server_manager.sub_command(name="информация", description="Информация о сервере.")
+    async def server_info(self, inter: disnake.ApplicationCommandInteraction):
         
+        timestamp = int(inter.guild.created_at.timestamp())
+        
+        embed = disnake.Embed(color=0x5865f1)
+        embed.set_author(name=f"Информация о {inter.guild.name}", icon_url=inter.guild.icon)
+        embed.set_thumbnail(url=inter.guild.icon.url if inter.guild.icon else disnake.Embed.Empty)
+        
+        embed.add_field(
+            name="Владелец:",
+            value=f"<:OWNER:1362561676213354606> {inter.guild.owner.mention}",
+            inline=True
+        )
+        
+        embed.add_field(
+            name="ID:",
+            value=f"<:SERVER:1362562819425308672> `{inter.guild.id}`",
+            inline=True
+        )
+        
+        embed.add_field(
+            name="Создан:",
+            value=f"<:CREATED:1362564229571285175> <t:{timestamp}:F> (<t:{timestamp}:R>)",
+            inline=False
+        )
+        
+        await inter.response.send_message(embed=embed, ephemeral=True)
+
 
 def setup(bot):
     bot.add_cog(General(bot))
