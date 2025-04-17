3db343d7f9701c354c902d734d512371f0baaf47
Daniil Waysman <daniil.waysman@gmail.com>
2025-04-18 00:15:00 +0200

    Апдейт main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import disnake
+from disnake.ext import commands
+import config
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+intents = disnake.Intents.all()
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Ready To Work!")
+
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
