bb8ad01e7a8004dd3fdca71b03f9f878e07131a7
Daniil Waysman <daniil.waysman@gmail.com>
2025-04-20 03:31:24 +0200

    Апдейт Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -5,61 +5,47 @@ import os
 class Database:
     def __init__(self, db_path="Database/database.db"):
         self.db_path = db_path
-        self._create_tables()
+        self._create_table()
 
     def _connect(self):
         return sqlite3.connect(self.db_path)
 
-    def _create_tables(self):
+    def _create_table(self):
         with self._connect() as db:
             cursor = db.cursor()
-
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS mutes (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                user_id INTEGER NOT NULL,
-                reason TEXT NOT NULL,
-                duration INTEGER,
-                adm INTEGER NOT NULL
-            )
-            """)
 
             cursor.execute("""
             CREATE TABLE IF NOT EXISTS recruit (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                message_id INTEGER,
                 user_id INTEGER NOT NULL,
-                nickname TEXT NOT NULL,
                 name TEXT NOT NULL,
-                age INTEGER NOT NULL
+                nickname TEXT NOT NULL,
+                age INTEGER NOT NULL,
+                info TEXT NOT NULL,
+                status TEXT DEFAULT 'Waiting'
             )
             """)
 
             db.commit()
-
-    def give_mute(self, user_id: int, reason: str, duration: int, adm: int):
-        with self._connect() as db:
-            cursor = db.cursor()
-            cursor.execute("""
-            INSERT INTO mutes (user_id, reason, duration, adm)
-            VALUES (?, ?, ?, ?)
-            """, (user_id, reason, duration, adm))
-            db.commit()
             
-                    
-    def add_recruit_info(self, user_id: str, nickname: str, name: str, age: int) -> bool:
+    def add_recruit(self, message_id: int, user_id: int, nickname: str, name: str, age: int, info: str, status: str = "Waiting") -> bool:
         with self._connect() as db:
             cursor = db.cursor()
-
-            cursor.execute("SELECT * FROM recruit WHERE user_id = ?", (user_id,))
-            result = cursor.fetchone()
-
-            if result:
-                return False
-
-            cursor.execute(""" 
-                INSERT INTO recruit (user_id, nickname, name, age)
-                VALUES (?, ?, ?, ?)
-            """, (user_id, nickname, name, age))
+            
+            cursor.execute("""
+                INSERT INTO recruit (message_id, user_id, nickname, name, age, info, status)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, (message_id, user_id, nickname, name, age, info, status))
+            
             db.commit()
             return True
+    
+    def check_recruit_exists(self, user_id: int, status: str = "Waiting") -> bool:
+        with self._connect() as db:
+            cursor = db.cursor()
+            
+            cursor.execute("SELECT 1 FROM recruit WHERE user_id = ? AND status = ?", (user_id, status))
 
+            result = cursor.fetchone()
+            return result is not None
