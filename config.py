9a7e8bb1ae673966675bad8302f35e22f62956b7
Daniil Waysman <daniil.waysman@gmail.com>
2025-04-26 04:15:05 +0200

    Апдейт config.py

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
-PREFIX="!"
-OWNER_ID=1220809080499605526
+import json
+
+def load_config():
+    with open("config.json", "r") as f:
+        return json.load(f)
+
+def save_config(data):
+    with open("config.json", "w") as f:
+        json.dump(data, f, indent=4)
+
+config_data = load_config()
+PREFIX = config_data["PREFIX"]
+OWNER_ID = config_data["OWNER_ID"]
